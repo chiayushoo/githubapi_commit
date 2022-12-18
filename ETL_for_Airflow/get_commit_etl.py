c2721410b104a421197a023a5f09568de88116d3
ChiaChia H <25942364+chiayushoo@users.noreply.github.com>
2022-12-18 14:31:50 +0800

    update start date

--- ETL_for_Airflow/get_commit_etl.py
+++ ETL_for_Airflow/get_commit_etl.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 # default arguments for the DAG
 default_args = {
     'owner': 'me',
-    'start_date': datetime(2022, 1, 1),
+    'start_date': datetime(2022, 12, 17),
     'depends_on_past': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5)
